4064fca4f09559ac06d37d455c1347cc6f2efc85
Versaillante <160363941+Versaillante@users.noreply.github.com>
2025-05-04 07:28:48 -0400

    ugh

---
+++ page_1.gd
@@ -0,0 +1,33 @@
+extends Sprite2D
+
+func _ready():
+	Input.set_custom_mouse_cursor(null)
+	show()
+	
+	add_child($hint1_1)
+	$hint1_1.pressed.connect(_hint1_1)
+	
+	add_child($hint1_2)
+	$hint1_2.pressed.connect(_hint1_2)
+	
+	add_child($hint1_3)
+	$hint1_3.pressed.connect(_hint1_3)
+	
+	add_child($hint1_4)
+	$hint1_4.pressed.connect(_hint1_4)
+	
+	add_child($hint1_5)
+	$hint1_5.pressed.connect(_hint1_5)
+
+	
+func _hint1_1():
+	print("connect")
+	$Label1_1.show()
+func _hint1_2():
+	$Label1_1.show()
+func _hint1_3():
+	$Label1_3.show()
+func _hint1_4():
+	$Label1_4.show()
+func _hint1_5():
+	$Label1_5.show()
---
+++ page_4.gd
@@ -0,0 +1,29 @@
+extends Sprite2D
+
+func _ready():
+	Input.set_custom_mouse_cursor(null)
+	show()
+	
+	add_child($hint1_1)
+	$hint1_1.pressed.connect(_hint1_1)
+	
+	add_child($hint1_2)
+	$hint1_2.pressed.connect(_hint1_2)
+	
+	add_child($hint1_3)
+	$hint1_3.pressed.connect(_hint1_3)
+	
+	add_child($hint1_4)
+	$hint1_4.pressed.connect(_hint1_4)
+	
+
+	
+func _hint1_1():
+	print("connect")
+	$Label1_1.show()
+func _hint1_2():
+	$Label1_2.show()
+func _hint1_3():
+	$Label1_3.show()
+func _hint1_4():
+	$Label1_4.show()
